359912eff6338efc5b1aa29d914c0f977a924716
Vnice85 <ducvan85204@gmail.com>
2025-01-04 01:07:41 +0700

    fix admin

--- MyWeb/Controllers/AdminController.cs
+++ MyWeb/Controllers/AdminController.cs
@@ -71,7 +71,7 @@ namespace WebApplication2.Controllers
             return View(content);
 
         }
-        [HttpPost]
+ 
         public ActionResult Delete(List<int> id)
         {
             foreach (var item in id)
@@ -80,13 +80,7 @@ namespace WebApplication2.Controllers
                 db.Contents.Remove(x);
                 db.SaveChanges();
             }
-            return PartialView("Show", db.Contents.OrderByDescending(x => x.id).ToList());
-        }
-
-        public ActionResult Index2()
-        {
-
-            return View(db.Contents.OrderByDescending(c => c.id).ToList());
+            return RedirectToAction("index");
         }
 
         public ActionResult AddGalleryImage()
@@ -109,7 +103,77 @@ namespace WebApplication2.Controllers
             }
             return View();
         }
+        public PartialViewResult Find(string find_keyword)
+        {
+            var x = (from a in db.Contents
+                     join b in db.Colors
+                     on a.id_color equals b.id_color
+                     where b.color_name.Contains(find_keyword.Trim()) || a.main_content.Contains(find_keyword.Trim()) || a.date_upload.ToString().Contains(find_keyword.Trim())
+                     orderby a.date_upload descending
+                     select new
+                     {
+                         a.id,
+                         b.id_color,
+                         a.content1,
+                         a.main_content,
+                         a.main_image,
+                         a.date_upload,
+                     }).ToList();
 
+            var ls = x.Select(z => new WebApplication2.Models.Content
+            {
+                id = z.id,
+                id_color = z.id_color,
+                content1 = z.content1,
+                main_content = z.main_content,
+                main_image = z.main_image,
+                date_upload = z.date_upload
+            });
+            return PartialView(ls.ToList());
+        }
+
+        public PartialViewResult Filter(int? page, int? size, string find_keyword, int orderby = 0)
+        {
+            find_keyword = string.IsNullOrEmpty(find_keyword) ? string.Empty : find_keyword.Trim();
+            int pageNumber = page ?? 1;
+            int pageSize = size ?? 10;
+            var query = (from a in db.Contents
+                         join b in db.Colors
+                         on a.id_color equals b.id_color
+                         where b.color_name.Contains(find_keyword)
+                         || a.main_content.Contains(find_keyword)
+                         || a.date_upload.ToString().Contains(find_keyword)
+                         || string.IsNullOrEmpty(find_keyword)
+                         select new
+                         {
+                             a.id,
+                             b.id_color,
+                             a.content1,
+                             a.main_content,
+                             a.main_image,
+                             a.date_upload,
+                         });
+            var result = query.ToList().Select(z => new WebApplication2.Models.Content
+            {
+                id = z.id,
+                id_color = z.id_color,
+                content1 = z.content1,
+                main_content = z.main_content,
+                main_image = z.main_image,
+                date_upload = z.date_upload
+            });
+            if (orderby == 0)
+            {
+                Session["filter"] = 0;
+                result = result.OrderByDescending(x => x.date_upload);
+            }
+            if (orderby != 0)
+            {
+                Session["filter"] = 1;
+                result = result.OrderBy(x => x.date_upload);
+            }
+            return PartialView(result.ToPagedList(pageNumber, pageSize));
+        }
 
 
         protected override void Dispose(bool disposing)
